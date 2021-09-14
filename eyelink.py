9390e9643b839ff2d7445c47098280ae499beeca
Benedikt Ehinger <behinger@uos.de>
2021-09-14 09:55:49 +0200

    python 3.8 + some fixes

--- eyelink.py
+++ eyelink.py
@@ -1,22 +1,24 @@
 '''
 Created on Jul 10, 2012
 Updated Aug 17, 2016
+Updated September 2021 by Michael Bui & Benedikt Ehinger
 @author: SCCN
 '''
 
-
 from pylink import *
-import pylsl,socket, time
+import socket
+import time
 import msvcrt
+from pylsl import StreamInfo, StreamOutlet
+import pylsl
 
-if __name__ == '__main__':
 
-    outlet = None 
-    SR = 500
-    edfFileName = "TRIAL.edf"
+def eye_link_lsl(edfFileName="TRIAL.edf",SR=500):
+
+    outlet = None
     try:
-        #info = pylsl.stream_info("EyeLink","Gaze",9,500,pylsl.cf_float32,"eyelink-" + socket.gethostname());
-        info = pylsl.stream_info("EyeLink","Gaze",10,SR,pylsl.cf_double64,"eyelink-" + socket.gethostname());
+        # info = pylsl.stream_info("EyeLink","Gaze",9,500,pylsl.cf_float32,"eyelink-" + socket.gethostname());
+        info = StreamInfo("EyeLink", "Gaze", 10, SR, 'double64', "eyelink-" + socket.gethostname())
         channels = info.desc().append_child("channels")
 
         channels.append_child("channel") \
@@ -51,55 +53,52 @@ if __name__ == '__main__':
             .append_child_value("label", "LSL_timestamp") \
             .append_child_value("type", "eyetracking")
 
-
-
-            
-        outlet = pylsl.stream_outlet(info)
-        print "Established LSL outlet."
+        outlet = StreamOutlet(info)
+        print("Established LSL outlet.")
     except:
-        print "Could not create LSL outlet."
-    
+        print("Could not create LSL outlet.")
 
     quit = 0
-    isVerbose = 0 
-    while quit!= 1 :
+
+    while quit != 1:
         try:
-            print "Trying to connect to EyeLink tracker..."
-            try:
-                tracker = EyeLink("255.255.255.255")
-                print "Established a passive connection with the eye tracker."
-            except:
-                tracker = EyeLink("100.1.1.1")
-                print "Established a primary connection with the eye tracker."
+            
+            if isConnected()==0:
+                print("Trying to connect to EyeLink tracker...")
+                try:
+                    tracker = EyeLink("255.255.255.255")
+                    print("Established a passive connection with the eye tracker.")
+                except:
+                    tracker = EyeLink("100.1.1.1")
+                    print("Established a primary connection with the eye tracker.")
             # uncomment if you want to get pupil size in diameter, otherwise it is the area
-            # tracker.setPupilSizeDiameter(YES)  
+            # tracker.setPupilSizeDiameter(YES)
             # tracker.setVelocityThreshold(22)
             beginRealTimeMode(100)
-            getEYELINK().openDataFile(edfFileName)	
-            getEYELINK().startRecording(1, 1, 1, 1)                
-            print "Now reading samples..."
-            print "Press \'Esc\' to quit"
 
-            while quit != 1: 
+            getEYELINK().openDataFile(edfFileName)
+            getEYELINK().startRecording(1, 1, 1, 1)
+            print("Now reading samples...")
+            print("Press \'Esc\' to quit")
+
+            while quit != 1:
                 sample = getEYELINK().getNewestSample()
-                quit = getEYELINK().escapePressed();
+                quit = getEYELINK().escapePressed()
                 if sample is not None:
                     now = pylsl.local_clock()
                     ppd = sample.getPPD()
-                    #values = [0,0, 0,0, 0, 0, sample.getTargetX(),sample.getTargetY(),sample.getTargetDistance(), ppd[0],ppd[1]]
-                    values = [0,0, 0,0, 0, 0, ppd[0],ppd[1], sample.getTime(), now]
+                    # values = [0,0, 0,0, 0, 0, sample.getTargetX(),sample.getTargetY(),sample.getTargetDistance(), ppd[0],ppd[1]]
+                    values = [0, 0, 0, 0, 0, 0, ppd[0], ppd[1], sample.getTime(), now]
                     if (sample.isLeftSample()) or (sample.isBinocular()):
                         values[0:2] = sample.getLeftEye().getGaze()
-                        values[4] = sample.getLeftEye().getPupilSize() 
+                        values[4] = sample.getLeftEye().getPupilSize()
                     if (sample.isRightSample()) or (sample.isBinocular()):
                         values[2:4] = sample.getRightEye().getGaze()
                         values[5] = sample.getRightEye().getPupilSize()
-                        
 
-                    
                     outlet.push_sample(pylsl.vectord(values), now, True)
-                    time.sleep(1.0/SR)
-                    
+                    time.sleep(1.0 / SR)
+
                     x = msvcrt.kbhit()
                     if x:
                         # getch acquires the character encoded in binary ASCII
@@ -107,18 +106,16 @@ if __name__ == '__main__':
                         if msvcrt.getch() == chr(27):
                             quit = 1
 
-        
-                    if quit==1 :
-                        getEYELINK().setOfflineMode()                         
-                        msecDelay(500);                 
+                    if quit == 1:
+                        getEYELINK().setOfflineMode()
+                        msecDelay(500)
 
                         # Close the file and transfer it to Display PC
                         getEYELINK().closeDataFile()
                         getEYELINK().receiveDataFile(edfFileName, edfFileName)
-                        getEYELINK().close();    
-        
-        except Exception, e:		
-            print "connection broke off: ", e
+                        getEYELINK().close()
 
-        
-    
+        except Exception as e:
+            print("connection broke off: ", e)
+
+
